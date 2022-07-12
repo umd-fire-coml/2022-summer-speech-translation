46ae4cd953875cbe84f43216b7b23c7d6e78c436
Steven Zhang <50186662+sbeven@users.noreply.github.com>
2022-07-12 17:06:14 -0400

    setup app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # have to run this locally as streamlit run app.py
 import streamlit as st
-from translation_test import *
+from TestTranslation.translation_test import *
 st.title("Translation model test")
 input_sentence = st.text_input("Enter input sentence:")
 if input_sentence is not None and len(input_sentence) > 0:
