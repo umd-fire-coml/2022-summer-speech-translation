40057407ba0c7038ae8605f374ed8cfd67477c06
Steven Zhang <50186662+sbeven@users.noreply.github.com>
2022-07-14 10:52:12 -0400

    ui update

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 # have to run this locally as streamlit run app.py
 import streamlit as st
-from TestTranslation.translation_test import *
+
+from TestTranslation.translation import *
+
+
+
 st.title("Translation model test")
-input_sentence = st.text_input("Enter input sentence:")
-if input_sentence is not None and len(input_sentence) > 0:
-    translated = decode_sequence(input_sentence)
-    st.write(translated)
-    input_sentence = None
+
+option = st.selectbox("Select input type:", ("text input", "audio input"))
+if option == "text input":
+    input_sentence = st.text_input("Enter input sentence:")
+    if input_sentence is not None and len(input_sentence) > 0:
+        translated = decode_sequence(input_sentence)
+        st.write(translated)
+        input_sentence = None
+else:
+    wav_sentence = st.file_uploader("Upload a wav file:")
+    st.button("Submit wav file")
+
+
