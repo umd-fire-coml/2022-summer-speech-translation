ffd9653aafe4aff7882183570f72ee07490f213f
Steven Zhang <50186662+sbeven@users.noreply.github.com>
2022-07-19 10:38:19 -0400

    chinese update

--- app.py
+++ app.py
@@ -2,20 +2,28 @@
 import streamlit as st
 from Autocorrect.autocorrectreal import edit
 from TestTranslation.translation import *
-
+from TestTranslationChinese.translation_model import decode_sequence_chinese
 
 
 st.title("Translation model test")
 
 option = st.selectbox("Select input type:", ("text input", "audio input"))
+option2 = st.selectbox("Select translation language:", ("Spanish", "Chinese"))
 if option == "text input":
     input_sentence = st.text_input("Enter input sentence:")
     if input_sentence is not None and len(input_sentence) > 0:
-        edited = edit(input_sentence)
-        st.write("Autocorrected sentence: " + edited)
-        translated = decode_sequence(edited)[8:-5]
-        st.write(translated)
-        input_sentence = None
+        if option2 == "Spanish":
+            edited = edit(input_sentence)
+            st.write("Autocorrected sentence: " + edited)
+            translated = decode_sequence(edited)[8:-5]
+            st.write(translated)
+            input_sentence = None
+        else:
+            edited = edit(input_sentence)
+            st.write("Autocorrected sentence: " + edited)
+            translated = decode_sequence_chinese(edited)[8:]
+            st.write(translated)
+            input_sentence = None
 else:
     wav_sentence = st.file_uploader("Upload a wav file:")
     st.button("Submit wav file")
